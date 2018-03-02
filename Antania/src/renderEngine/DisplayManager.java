c5fd9c6c0b13c880e2c87dee8c0e8500a71d782b
Furyyy502 <36983527+Furyyy502@users.noreply.github.com>
2018-03-02 09:59:55 +0000

    Update DisplayManager.java

--- Antania/src/renderEngine/DisplayManager.java
+++ Antania/src/renderEngine/DisplayManager.java
@@ -9,10 +9,14 @@ public class DisplayManager {
 	
 	private static final int WIDTH = 1280;
 	private static final int HEIGHT = 720;
+	private static final int FPS_CAP = 120;
 	
 	public static void createDisplay(){
 		
 		ContextAttribs attribs = new ContextAttribs(3,2);
+		attrubs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+	
 		
 		try {
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
@@ -22,15 +26,19 @@ public class DisplayManager {
 			e.printStackTrace();	
 		}
 		
+		GL11.glview(0, 0, WIDTH, HEIGHT);
 	}
 	
 	public static void updateDisplay(){
 		
+		Display.syn(FPS_CAP);
+		Display.uodate();
 		
 	}
 	
 	public static void closeDisplay(){
 		
+		Display.destroy();
 		
 	}
 	
